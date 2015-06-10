ad60f3220506e4fa3cbef144b8e4bbe98aa16937
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2015-06-10 12:34:53 -1000

    first commit

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -2,7 +2,7 @@
 # 201506-Philippe.eychart@informatique.gov.pf
 # licence GNU-v3
 set -e
-hook_name=$(basename $(echo $0|grep -v bash))
+hook_name=$(basename $(echo $0| grep -v bash))
 hook_name=${hook_name:="hook.sh"}
 cd $(dirname $0)
 mytmp="/tmp/.${hook_name}$(date +%Y%m%d.%H%M%S)"
@@ -18,7 +18,7 @@ config-get() { shift
  case $1 in
   snmp_community)	echo "public";;
   snmp_allowed_ips)	echo '["192.168.10.100", "10.0.0.0/8"]';;
-  refresh_patterns)	echo '{"http://www.ubuntu.com": {min: 0, percent: 20, max: 60}, "http://www.canonical.com": {min: 0, percent: 20, max: 120}}';;
+  refresh_patterns)	echo '{"http://www.ubuntu.com": {"min": 0, "percent": 20, "max": 60}, "http://www.canonical.com": {"min": 0, "percent": 20, "max": 120}}';;
   cache_dir)		echo '/var/spool/squid3';;
   max_obj_size_kb)	echo '8192';;
   cache_size_mb)	echo '512';;
@@ -26,6 +26,7 @@ config-get() { shift
   target_objs_per_dir)	echo '400';;
   avg_obj_size_kb)	echo '16';;
   log_format)		echo '%>a %ui %un [%tl] "%rm %ru HTTP/%rv" %>Hs %<st "%{Referer}>h" "%{User-Agent}>h" %Ss:%Sh';;
+  auth_list)		echo '[{"dstdomain": ["www.ubuntu.com"], "src": ["1.2.3.4", "5.6.7.0/24"]}, {"url_regex": ["https?://[^/]+[.]internal(/.*)?"], "src": ["192.168.0.0/16"]}]';;
   *) false
  esac
 }
@@ -36,13 +37,12 @@ config-get() { shift
 ################################################################################
 open_port()    { [ -z "$(which open-port)"  ] || for i in $*; do open-port  $i; done; }
 close_port()   { [ -z "$(which open-close)" ] || for i in $*; do close-port $i; done; }
-format_jshon() { sed -e 's/\([^"A-Za-z]\)\([A-Za-z][A-Za-z]*\):/\1"\2":/g'; }
 get_config()   { config-get '--format=json' $1; }
 service_ports(){ [ -z "$1" ] || egrep -w "http_port" $1| egrep -w "([0-9]+)"; }
-get_json_keys(){ [ -z "$*" ] || echo "$*"| format_jshon| jshon -k; }
-get_json_len() { [ -z "$*" ] || echo "$*"| format_jshon| jshon -l; }
-get_json()     { 
- [ -z "$*" ] || format_jshon| jshon $(while [ $# -ne 0 ]; do echo "-e $1"; shift; done)
+get_json_keys(){ jshon -k || true; }
+get_json_len() { jshon -l || true; }
+get_json()     { #Syntaxe: echo <String>| get_json <key>  # cf. man jshon
+ [ -z "$*" ] || jshon $(while [ $# -ne 0 ]; do echo "-e $1"; shift; done)| sed -e 's;\\/;/;g'
 }
 
 cache_l1() { local r
@@ -50,6 +50,12 @@ cache_l1() { local r
  r=$(expr $r / $(get_config target_objs_per_dir))
  r=$(expr $r / $(get_config avg_obj_size_kb))
  expr $r \* $r
+}
+
+auth_list_parser() { local v=$(cat)
+ for i in $(seq 0 $(get_config auth_list)); do
+  echo $i
+ done
 }
 
 apt_get_install() {
@@ -60,15 +66,11 @@ apt_get_install() {
 ################################################################################
 # End "Supporting functions"
 ################################################################################
-resource_template() { local dest=$2; [ -z "$2" ] || dest=">$dest"
+resource_template() {
  <$hook_name awk '{ if ( $0 ~ /resource_template()/ ) bool=1; if (!bool) print $0 }' >$mytmp
- cat >>$mytmp <<EOF
-cat $dest <<@@@
-EOF
+ echo "cat $([ -z "$2" ] || echo '>')$2 <<@@@" >>$mytmp
  cat >>$mytmp <../templates/$1
- cat >>$mytmp <<EOF
-@@@
-EOF
+ echo "@@@" >>$mytmp
  [ 0$DEBUG -ne 0 ] && less $mytmp
  $SHELL $mytmp
  [ 0$DEBUG -ne 0 ] && less $2
@@ -83,7 +85,11 @@ install_hook() {
  [ -z "$(which jshon)" ] && apt_get_install jshon
  [ -z "$(which jshon)" ] && juju-log "the \"jshon\" command is not installed!" && exit 1
  [ 0$DEBUG -eq 0 ] && apt_get_install squid
- resource_template "main_config.template" "/tmp/squid.conf"
+
+ [ -d $default_squid3_config_dir ] || mkdir -p $default_squid3_config_dir
+ [ ! -e $default_squid3_config ] || \
+   cp -pf $default_squid3_config $default_squid3_config.$(date +%y%m%d.%H%M%S)
+ resource_template "main_config.template" $default_squid3_config
 }
 
 config_changed() {
