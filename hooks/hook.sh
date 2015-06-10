32ca71b1db2cfb02872303d4e24686d0abc368f5
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2015-06-09 17:38:44 -1000

    first commit

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -13,14 +13,23 @@ default_squid3_config=${default_squid3_config_dir}"/squid.conf"
 default_squid3_config_cache_dir="/var/run/squid3"
 
 DEBUG=1
+# JUJU ENV... (to comment)
 config-get() { shift
  case $1 in
   snmp_community)	echo "public";;
   snmp_allowed_ips)	echo '["192.168.10.100", "10.0.0.0/8"]';;
   refresh_patterns)	echo '{"http://www.ubuntu.com": {min: 0, percent: 20, max: 60}, "http://www.canonical.com": {min: 0, percent: 20, max: 120}}';;
+  cache_dir)		echo '/var/spool/squid3';;
+  max_obj_size_kb)	echo '8192';;
+  cache_size_mb)	echo '512';;
+  cache_mem_mb)		echo '256';;
+  target_objs_per_dir)	echo '400';;
+  avg_obj_size_kb)	echo '16';;
+  log_format)		echo '%>a %ui %un [%tl] "%rm %ru HTTP/%rv" %>Hs %<st "%{Referer}>h" "%{User-Agent}>h" %Ss:%Sh';;
   *) false
  esac
 }
+# End JUJU ENV (to comment)
 
 ################################################################################
 # Supporting functions
@@ -34,6 +43,14 @@ get_json_keys(){ [ -z "$*" ] || echo "$*"| format_jshon| jshon -k; }
 get_json_len() { [ -z "$*" ] || echo "$*"| format_jshon| jshon -l; }
 get_json()     { 
  [ -z "$*" ] || format_jshon| jshon $(while [ $# -ne 0 ]; do echo "-e $1"; shift; done)
+}
+
+cache_l1() { local r
+ r=$(expr $(get_config cache_size_mb) \* 64 )
+ r=$(expr $r / $(get_config target_objs_per_dir))
+ r=$(expr $r / $(get_config avg_obj_size_kb))
+ r=$(expr $r \* $r)
+ echo $r
 }
 
 apt_get_install() {
