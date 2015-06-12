3f89293e09af59c92f0a0d36508447751dc1b6de
root <root@maas.maas>
2015-06-11 19:45:36 -1000

    First commit

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -14,26 +14,8 @@ default_squid3_config=${default_squid3_config_dir}"/squid.conf"
 default_squid3_config_cache_dir="/var/run/squid3"
 
 ############################### TO REMOVE ######################################
-DEBUG=1
+DEBUG=0
 nop() { echo -e "\c"; }
-# JUJU ENV... (to comment)
-config-get() { shift
- case $1 in
-  snmp_community)	echo "public";;
-  snmp_allowed_ips)	echo '["192.168.10.100", "10.0.0.0/8"]';;
-  refresh_patterns)	echo '{"http://www.ubuntu.com": {"min": 0, "percent": 20, "max": 60}, "http://www.canonical.com": {"min": 0, "percent": 20, "max": 120}}';;
-  cache_dir)		echo '/var/spool/squid3';;
-  max_obj_size_kb)	echo '8192';;
-  cache_size_mb)	echo '512';;
-  cache_mem_mb)		echo '256';;
-  target_objs_per_dir)	echo '400';;
-  avg_obj_size_kb)	echo '16';;
-  log_format)		echo '%>a %ui %un [%tl] "%rm %ru HTTP/%rv" %>Hs %<st "%{Referer}>h" "%{User-Agent}>h" %Ss:%Sh';;
-  auth_list)		echo '[{"dstdomain": ["www.ubuntu.com"], "src": ["1.2.3.4", "5.6.7.0/24"]}, {"url_regex": ["https?://[^/]+[.]internal(/.*)?"], "src": ["192.168.0.0/16"]}]';;
-  *) false
- esac
-}
-# End JUJU ENV (to comment)
 ############################### END "TO REMOVE" ################################
 
 ################################################################################
@@ -41,13 +23,13 @@ config-get() { shift
 ################################################################################
 open_port()    { [ -z "$(which open-port)"  ] || for i in $*; do open-port  $i; done; }
 close_port()   { [ -z "$(which open-close)" ] || for i in $*; do close-port $i; done; }
-get_config()   { config-get '--format=json' $1; }
 service_ports(){ [ -z "$1" ] || egrep -w "http_port" $1| egrep -w "([0-9]+)"; }
 get_json_keys(){ jshon -k || true; }
 get_json_len() { jshon -l || true; }
 get_json()     { #Syntaxe: echo <String>| get_json <key>  # cf. man jshon
- [ -z "$*" ] || jshon $(while [ $# -ne 0 ]; do echo "-e $1"; shift; done)| sed -e 's;\\/;/;g'
+ [ -z "$*" ] || jshon $(while [ $# -ne 0 ]; do echo "-e $1"; shift; done)| sed -e 's;\\/;/;g' -e 's/^"//' -e 's/"$//'
 }
+get_config()   { config-get '--format=json'| get_json $1; }
 
 cache_l1() { local r
  r=$(expr $(get_config cache_size_mb) \* 64 )
@@ -98,12 +80,12 @@ relation_get_all() { local reldata relid unit
 install_hook() {
  [ -z "$(which jshon)" ] \
    && juju_log "installing packages" \
-   && DEBIAN_FRONTEND=noninteractive apt_get -y install jshon
+   && DEBIAN_FRONTEND=noninteractive apt-get -y install jshon
  [ -z "$(which jshon)" ] && juju_log "the \"jshon\" command is not installed!" && exit 1
 
  [ 0$DEBUG -eq 0 ] \
    && juju_log "installing packages" \
-   && DEBIAN_FRONTEND=noninteractive apt_get -y install squid
+   && DEBIAN_FRONTEND=noninteractive apt-get -y install squid
 
  [ -d $default_squid3_config_dir ] || mkdir -p $default_squid3_config_dir
  [ ! -e $default_squid3_config ] || \
